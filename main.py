4821ad894fc2cd15bd4b363d59ccff843a02a2b8
rvnthh88 <revanth89005@gmail.com>
2025-11-21 05:50:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,44 @@ finally:
 
 
 week 12 --- https://github.com/Srivaishnavi08/tests.git
+week 7 --- https://github.com/docker/getting-started-app.git
+FROM node:18-alpine
+WORKDIR /app
+COPY . .
+RUN yarn install --production
+CMD ["node", "src/index.js"]
+EXPOSE 3000
 
+
+index.html
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <button id="btn" onclick="document.getElementById('msg').innerText=('HELLO')">
+            CLICK FOR SUPRISE
+        </button>
+        <p id="msg"></p>
+    </body>
+</html>
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+chrome_service = Service("C:\\Users\\REVANTH\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=chrome_service)
+
+try:
+    driver.get("file:///C:/devexternal/week11/index.html")
+
+    driver.find_element(By.ID, "btn").click()
+    time.sleep(20)
+
+    msg = driver.find_element(By.ID, "msg").text
+    print("Output:", msg)
+
+finally:
+    driver.quit()
